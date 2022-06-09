b25c3a60b4f7bba63c59fc0d2c0847dfbccb03f2
Niklavs Romans <niklavsromans19@gmail.com>
2022-06-09 17:17:26 +0200

    added components

---
+++ src/components/filterbuttons/FilterButtons.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="single-filter-button">
+    <button class="joke-generate-btn" value="Any" @click="changeFilter">
+      Generate Random Jokes
+    </button>
+  </div>
+  <div class="filter-buttons-box">
+    <button
+      class="filter-btn"
+      @click="changeFilter"
+      :key="title"
+      :value="category"
+      v-for="{ title, category } in filterButtons"
+    >
+      {{ title }}
+    </button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "FilterButtons",
+  data: () => {
+    return {
+      filterButtons: [
+        {
+          title: "Programming jokes",
+          category: "Programming",
+        },
+        {
+          title: "Misc jokes",
+          category: "Misc",
+        },
+        {
+          title: "Dark jokes",
+          category: "Dark",
+        },
+        {
+          title: "Pun jokes",
+          category: "Pun",
+        },
+      ],
+    };
+  },
+  methods: {
+    changeFilter(e) {
+      this.$store.dispatch("RandomJokes/getData", e.target.value);
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import "FilterButtons.scss";
+</style>
---
+++ src/components/jokelist/JokeList.vue
@@ -0,0 +1,30 @@
+<template>
+  <li class="joke-item" @click="clickHandler">
+    {{ jokeText }}
+    <div class="joke-category-box">
+      <span> {{ category }}</span>
+    </div>
+  </li>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: '"JokeList"',
+  data() {
+    return {};
+  },
+  emits: ["onClick"],
+  props: ["jokeText", "category"],
+  methods: {
+    clickHandler() {
+      this.$emit("onClick");
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import "JokeList.scss";
+</style>
